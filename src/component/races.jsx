92a60b495fa69b97a036952d11b996fbb5bb0aaf
unknown <kamranalisheeno9@gmail.com>
2021-06-09 09:32:49 +0500

    modified

--- src/component/races.jsx
+++ src/component/races.jsx
@@ -16,24 +16,24 @@ const Races = () => {
 
     const raceData = [
         {
-            races:{name:"Race 1",dnf:"DNF 1",lap:2,lapxy:"1x1",dnfxy:"4x4",countdown:"Countdown Text"}
+            races: { name: "Race 1", dnf: "DNF 1", lap: 2, lapxy: "1x1", dnfxy: "4x4", countdown: "Countdown Text" }
 
         },
         {
-            races:{name:"Race 2",dnf:"DNF 2",lap:4,lapxy:"2x2",dnfxy:"3x3",countdown:"Countdown Text"}
+            races: { name: "Race 2", dnf: "DNF 2", lap: 4, lapxy: "2x2", dnfxy: "3x3", countdown: "Countdown Text" }
 
         },
         {
-            races:{name:"Race 3",dnf:"DNF 3",lap:8,lapxy:"3x3",dnfxy:"2x2",countdown:"Countdown Text"}
+            races: { name: "Race 3", dnf: "DNF 3", lap: 8, lapxy: "3x3", dnfxy: "2x2", countdown: "Countdown Text" }
 
         },
-        
+
         {
-            races:{name:"Race 4",dnf:"DNF 4",lap:12,lapxy:"4x4",dnfxy:"1x1",countdown:"Countdown Text"}
+            races: { name: "Race 4", dnf: "DNF 4", lap: 12, lapxy: "4x4", dnfxy: "1x1", countdown: "Countdown Text" }
 
         },
-        
-        
+
+
     ]
     const infoFunc = () => {
         setInfo(true)
@@ -54,7 +54,7 @@ const Races = () => {
         setLoadRace(true)
         setInfo(true)
     }
-    
+
     const Select = (k) => {
         setIdkey(k)
         setselectRace(true)
@@ -65,7 +65,7 @@ const Races = () => {
         setselectRace(false)
         setRegistration(true)
     }
-    
+
     const Unregister = () => {
         setLoadRace(true)
         setRegistration(false)
@@ -77,8 +77,8 @@ const Races = () => {
         setselectRace(true)
     }
 
-    
-    const DropdownShow =()=>{
+
+    const DropdownShow = () => {
         setDropdown(!Dropdown)
     }
 
@@ -114,16 +114,16 @@ const Races = () => {
                     </Col>
 
                 </Row>
-                        <button onClick={()=>DropdownShow()} className="dropdown btn">Select Map</button>
-                        <ul className={ Dropdown ? "items" : "hideinfo"}>
+                <button onClick={() => DropdownShow()} className="dropdown btn">Select Map</button>
+                <ul className={Dropdown ? "items" : "hideinfo"}>
                     {raceData.map((value, key) => {
                         return (
-                        
-                            <li key={key} onClick={()=>Select(key)}>{value.races.name}</li>
-                            )
-                        })}
-                        </ul>
-                
+
+                            <li key={key} onClick={() => Select(key)}>{value.races.name}</li>
+                        )
+                    })}
+                </ul>
+
             </Container>
 
 
@@ -131,60 +131,60 @@ const Races = () => {
                 <Row>
                     <Col xl={2} lg={2} md={2} sm={2} xs={2}>
 
-                        <button onClick={()=>Unregister()} > <FaAngleLeft />  </button>
+                        <button onClick={() => Unregister()} > <FaAngleLeft />  </button>
                     </Col>
 
-                </Row> 
-                  
+                </Row>
+
                 <Row className="test-race">
                     <Col xl={12} lg={12} md={12} sm={12} xs={12} className="test-col" >
-                    {raceData[Idkey].races.name}
+                        {raceData[Idkey].races.name}
                     </Col>
                     <Col xl={5} lg={5} md={5} sm={5} xs={5} className="test-col">
-                    {raceData[Idkey].races.lap}
-                   
-                    </Col>
-                    <Col xl={5} lg={5} md={5} sm={5} xs={5}  className="test-col">
-                    {raceData[Idkey].races.dnf}
+                        {raceData[Idkey].races.lap}
 
                     </Col>
-                    <Col xl={12} lg={12} md={12} sm={12} xs={12} onClick={()=>Register()} className="test-col" >
-                    Open Race Registration
+                    <Col xl={5} lg={5} md={5} sm={5} xs={5} className="test-col">
+                        {raceData[Idkey].races.dnf}
+
                     </Col>
-                    </Row>        
-           </Container>
-            <Container className={ Registration ? "info-container" :"hideinfo"} >
+                    <Col xl={12} lg={12} md={12} sm={12} xs={12} onClick={() => Register()} className="test-col" >
+                        Open Race Registration
+                    </Col>
+                </Row>
+            </Container>
+            <Container className={Registration ? "info-container" : "hideinfo"} >
                 <Row>
                     <Col xl={2} lg={2} md={2} sm={2} xs={2}>
 
                         <button onClick={() => Test()} > <FaAngleLeft />  </button>
                     </Col>
 
-                </Row>   
+                </Row>
                 <Row className="test-race">
                     <Col xl={12} lg={12} md={12} sm={12} xs={12} className="test-col" >
-                    {raceData[Idkey].races.name}
+                        {raceData[Idkey].races.name}
 
                     </Col>
-                    <Col xl={5} lg={5} md={5} sm={5} xs={5}  onClick={()=>Unregister()} className="test-col unregister">
-                    Unregister
+                    <Col xl={5} lg={5} md={5} sm={5} xs={5} onClick={() => Unregister()} className="test-col unregister">
+                        Unregister
                     </Col>
-                    <Col xl={6} lg={6} md={6} sm={6} xs={6}   className="test-col">
-                    <Row>
-                        <Col xl={12} lg={12} md={12} sm={12} xs={12}>
-                    LAPS : {raceData[Idkey].races.lapxy}
-                        
-                        </Col>
-                        <Col xl={12} lg={12} md={12} sm={12} xs={12}>
-                        DNF : {raceData[Idkey].races.dnfxy}
-                        </Col>
-                    </Row>
+                    <Col xl={6} lg={6} md={6} sm={6} xs={6} className="test-col">
+                        <Row>
+                            <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+                                LAPS : {raceData[Idkey].races.lapxy}
+
+                            </Col>
+                            <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+                                DNF : {raceData[Idkey].races.dnfxy}
+                            </Col>
+                        </Row>
                     </Col>
                     <Col xl={5} lg={5} md={5} sm={5} xs={5} className="test-col" >
-                    {raceData[Idkey].races.countdown}
+                        {raceData[Idkey].races.countdown}
                     </Col>
-                    </Row>        
-           </Container>
+                </Row>
+            </Container>
         </div>
     );
 }
